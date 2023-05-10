5f88131806a15add92d684d882c1899ce98ff595
fear-the-reaper <jta.reaper@gmail.com>
2023-05-10 18:01:56 +0500

    Create course controller

---
+++ server/src/controllers/courseController.js
@@ -0,0 +1,117 @@
+const course = require("../models/courseModel");
+
+// Create a new course
+const createCourse = async (req, res) => {
+  try {
+    const {
+      title,
+      overview,
+      description,
+      image,
+      syllabus,
+      deadline,
+      enrollmentLink,
+    } = req.body;
+    const course = new course({
+      title: title,
+      overview: overview,
+      description: description,
+      image: image,
+      syllabus: syllabus,
+      deadline: deadline,
+      enrollmentLink: enrollmentLink,
+    });
+
+    await course.save();
+    res.status(201).json({ message: "Course created successfully.", course });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error while creating course." });
+  }
+};
+
+// Get all courses
+const getAllCourses = async (req, res) => {
+  try {
+    const courses = await course.find();
+    res
+      .status(200)
+      .json({ message: "All courses found successfully.", courses });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Server error while getting all courses." });
+  }
+};
+
+// Get a course by id
+const getCourseById = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const course = await course.findById(courseId);
+    if (course) {
+      res.status(200).json({ message: "Course found successfully.", course });
+    } else {
+      res.status(404).json({ message: "Course not found." });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Server error while getting course by id." });
+  }
+};
+
+// Update a course by id
+const updateCourseById = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const {
+      title,
+      overview,
+      description,
+      image,
+      syllabus,
+      deadline,
+      enrollmentLink,
+    } = req.body;
+    const course = await course.findByIdAndUpdate(courseId, {
+      title: title,
+      overview: overview,
+      description: description,
+      image: image,
+      syllabus: syllabus,
+      deadline: deadline,
+      enrollmentLink: enrollmentLink,
+    });
+    res.status(200).json({ message: "Course updated successfully.", course });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Server error while updating course by id." });
+  }
+};
+
+// Delete a course by id
+const deleteCourseById = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const course = await course.findByIdAndDelete(courseId);
+    res.status(200).json({ message: "Course deleted successfully.", course });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Server error while deleting course by id." });
+  }
+};
+
+module.exports = {
+  createCourse,
+  getAllCourses,
+  getCourseById,
+  updateCourseById,
+  deleteCourseById,
+};
